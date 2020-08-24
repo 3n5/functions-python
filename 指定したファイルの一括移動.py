8c5b2ae8f34e84d485f03fb46eb418a58e93d94a
Takeuchi Haruhiko <noharu1205@gmail.com>
2020-08-24 19:06:34 +0900

    Add files via upload

---
+++ 指定したファイルの一括移動.py
@@ -0,0 +1,17 @@
+"""sample.txtの中のファイルをfolderへ移動する"""
+def file2folder_in_txt_lists():
+    import os
+    import glob
+    import shutil
+
+    folder="to"
+    target = '{}'.format(folder) + os.sep  # os.sep == /
+    for line in open('sample.txt', 'r'):
+        line = line.strip() 
+        print(line)   
+        files = glob.glob(line)
+        if len(files) == 1:
+            print(files[0])
+            shutil.move(files[0], target)
+
+
