bcca60c89dafd8ec7eeadb3f8ca7b735dd75a7ae
Takeuchi Haruhiko <noharu1205@gmail.com>
2020-12-23 11:37:11 +0900

    Update png2jpg.py

--- png2jpg.py
+++ png2jpg.py
@@ -1,17 +1,34 @@
-"""png to jpg"""
+"""Convert png to jpg"""
 import os
-import cv2
 import sys
-import numpy as np
 from PIL import Image
+import glob
+import time
 
-path_png = "C:/Users/USER/Downloads/download.png"
-path_jpg = "C:/Users/USER/Downloads/download.jpg"
+#Folder with png
+target_dir = r"C:/Users/USER/Desktop/png_folder" 
 
-image = Image.open(path_png).convert('P')
-image.save(path_png)
-rgb_im = Image.open(path_png).convert('RGB')
-rgb_im.save(path_jpg)
 
-print("fin")
+#make save folder
+dirname, basename = os.path.split(target_dir )
+save_dir=os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "/Desktop/"+basename+"_jpg/"
+if not os.path.exists(save_dir): 
+    os.makedirs(save_dir)
+
+#png 
+def png2jpg(path_png,path_jpg):
+    image = Image.open(path_png).convert('P')
+    image.save(path_png)
+    rgb_im = Image.open(path_png).convert('RGB')
+    rgb_im.save(path_jpg)
+
+files = glob.glob(target_dir + '/*')
+data=[]
+print(len(files)*0.1/60,' sec')
+for file in files:
+    dirname, basename = os.path.split(file) 
+    root, ext = os.path.splitext(basename)
+    path_jpg=save_dir+root+'.jpg'
+    png2jpg(file,path_jpg)
+    time.sleep(0.1)
 
